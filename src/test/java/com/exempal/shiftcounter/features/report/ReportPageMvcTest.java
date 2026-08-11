ad03cf6f42cf755282669a1849eb7e394b098db4
ts-ars <tseren.arsinov@gmail.com>
2026-08-11 17:00:06 +0200

    test: render report mvc template

--- src/test/java/com/exempal/shiftcounter/features/report/ReportPageMvcTest.java
+++ src/test/java/com/exempal/shiftcounter/features/report/ReportPageMvcTest.java
@@ -8,23 +8,34 @@ import com.exempal.shiftcounter.features.report.application.ReportSignalTotal;
 import com.exempal.shiftcounter.features.report.application.ReportView;
 import com.exempal.shiftcounter.features.comment.domain.LossCategory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.mock;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(PageController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@Import({PageModelResolver.class, ReportPage.class})
 class ReportPageMvcTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ReportQueryUseCase reports;
+
     @Test
     void mapsRangeSensorFiveRowsSourcesAndChartTotalsToTheMvcModel() throws Exception {
-        ReportQueryUseCase reports = mock(ReportQueryUseCase.class);
         when(reports.query(Map.of("from", "2026-08-09", "to", "2026-08-10", "sensorId", "sensor-5")))
                 .thenReturn(new ReportView(
                         List.of(new ReportRow("sensor-2", LossCategory.BREAKDOWN, 12, 48, "belt")),
@@ -34,9 +45,6 @@ class ReportPageMvcTest {
                                 new ReportSignalTotal("sensor-2", 20),
                                 new ReportSignalTotal("sensor-3", 30),
                                 new ReportSignalTotal("sensor-4", 40))));
-        MockMvc mvc = MockMvcBuilders.standaloneSetup(new PageController(
-                new PageModelResolver(List.of(new ReportPage(reports))))).build();
-
         mvc.perform(get("/page/report")
                         .param("from", "2026-08-09")
                         .param("to", "2026-08-10")
@@ -49,6 +57,14 @@ class ReportPageMvcTest {
                 .andExpect(model().attribute("sensorId", "sensor-5"))
                 .andExpect(model().attribute("problems", hasSize(1)))
                 .andExpect(model().attribute("signalTotals", hasSize(4)))
-                .andExpect(model().attribute("sensorOptions", hasSize(6)));
+                .andExpect(model().attribute("sensorOptions", hasSize(6)))
+                .andExpect(content().string(containsString("name=\"sensorId\"")))
+                .andExpect(content().string(containsString("sensor-6")))
+                .andExpect(content().string(allOf(
+                        containsString("report-charts"), containsString("sensor-five"))))
+                .andExpect(content().string(containsString(">Source</th>")))
+                .andExpect(content().string(containsString("sensor-2")))
+                .andExpect(content().string(containsString("id=\"lossChart\"")))
+                .andExpect(content().string(containsString("id=\"signalChart\"")));
     }
 }
