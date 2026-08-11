f47e40d2465566f13e81e14e2add3053c9d2fc01
ts-ars <tseren.arsinov@gmail.com>
2026-08-11 15:03:08 +0200

    feat: update settings as one atomic snapshot

--- src/main/java/com/exempal/shiftcounter/features/settings/application/UpdateSettingsGroupCommand.java
+++
@@ -1,6 +0,0 @@
-package com.exempal.shiftcounter.features.settings.application;
-
-import java.util.List;
-
-public record UpdateSettingsGroupCommand(String groupId, String name, boolean enabled,
-                                         List<String> hours, List<Integer> plans) {}
