847a6a1d65f25c9d1ed518cbb826bf3d7273c45a
ts-ars <tseren.arsinov@gmail.com>
2026-08-11 15:03:20 +0200

    feat: expose unified settings snapshot

--- src/main/java/com/exempal/shiftcounter/features/settings/adapter/web/SettingsRequest.java
+++ src/main/java/com/exempal/shiftcounter/features/settings/adapter/web/SettingsRequest.java
@@ -2,13 +2,6 @@ package com.exempal.shiftcounter.features.settings.adapter.web;
 
 import java.util.List;
 
-public record SettingsRequest(
-        String name,
-        Boolean enabled,
-        List<String> hours,
-        List<String> hourlyPlans
-) {
-    public SettingsRequest(List<String> hours, List<String> hourlyPlans) {
-        this(null, null, hours, hourlyPlans);
-    }
+public record SettingsRequest(List<String> hours, List<Integer> sensors1To4Plans,
+                              List<Integer> sensor6Plans) {
 }
